27f1a43b155e840545125bffa911699957983e30
VeetiJoensuu <152386560+VeetiJoensuu@users.noreply.github.com>
2024-11-07 21:29:11 +0200

    exercise 4 done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pkg from 'pg';
+import axios from 'axios';
 
 const port = 3001;
 const { Pool } = pkg;
--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Row from './components/Row';
+
+const url = 'http://localhost:3001'
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setTasks(response.data)
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error)
+    })
+  }, [])
+
   const addTask = () => {
-    setTasks([...tasks,task])
-    setTask('')
+    axios.post(url + '/create', {
+      description: task
+    })
+    .then(response => {
+      setTasks([...tasks, { id: response.data.id, description: task }]);
+      setTask('');
+    })
+    .catch(error => {
+      alert(error.response && error.response.data && error.response.data.error ? error.response.data.error : error.message);
+    });
+  };  
+
+  const deleteTask = (id) => {
+    axios.delete(url + '/delete/' + id)
+      .then(response => {
+        const withoutRemoved = tasks.filter((item) => item.id !== id)
+        setTasks(withoutRemoved)
+      }).catch(error => {
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
   }
 
-  const deleteTask = (deleted) => {
-      const withoutRemoved = tasks.filter((item) => item !== deleted)
-      setTasks(withoutRemoved)
-  }
-
-  return (
+return (
     <div id="container">
-        <h3>Todos</h3>
-        <form>
-          <input
-            placeholder='Add new task'
-            value={task}
-            onChange={e => setTask(e.target.value)}
-            onKeyDown={e => {
-              if (e.key === 'Enter') {
-                e.preventDefault()
-                addTask()
-              }
-            }}
-          />
-        </form>
-        <ul>
-          {
-            tasks.map(item => (
-              <li key={item}>
-                {item}
-                <button className='delete-button' onClick={() => deleteTask(item)}>Delete</button>
-              </li>
-            ))
-          }
-        </ul>
+      <h3>Todos</h3>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addTask();
+      }}>
+        <input
+          placeholder='Add new task'
+          value={task}
+          onChange={e => setTask(e.target.value)}
+        />
+      </form>
+      <ul>
+        {
+          tasks.map(item => (
+            <Row key={item.id} item={item} deleteTask={deleteTask}/>
+          ))
+        }
+      </ul>
     </div>
   );
 }
