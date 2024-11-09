529305c8db88a1017552aa9209a67fa19b407cc8
VeetiJoensuu <152386560+VeetiJoensuu@users.noreply.github.com>
2024-11-09 13:00:44 +0200

    exercise 6 done

---
+++ server/helpers/auth.js
@@ -0,0 +1,21 @@
+const authorizationRequired = "Authorization required"
+const invalidCreditentials = "Invalid creditentials"
+
+const auth = (req, res, next) => {
+    const authorizationRequired = 'Authorization required';
+    const invalidCredentials = 'Invalid credentials';
+
+    if (!req.headers.authorization) {
+        res.statusMessage = authorizationRequired;
+        res.status(401).json({ message: authorizationRequired });
+    } else {
+        try {
+            const token = req.headers.authorization;
+            jsonwebtoken.verify(token, process.env.JWT_SECRET_KEY);
+            next();
+        } catch (err) {
+            res.statusMessage = invalidCredentials;
+            res.status(403).json({ message: invalidCredentials });
+        }
+    }
+};
